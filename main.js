d30262b36ba2eba83c1e6a86a492d7bb94e41a22
Mohamed Abdelmaksoud <medohmada1623@gmail.com>
2024-01-08 07:51:35 +0200

    adding water and sun graphics to the scene

--- main.js
+++ main.js
@@ -1,93 +1,304 @@
-import { OrbitControls } from "THREE/examples/jsm/controls/OrbitControls.js";
-import { Raycaster } from "three";
+import * as THREE from "three";
 
-// Create a scene
-const scene = new THREE.Scene();
-// Create a camera
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
+import Stats from "three/addons/libs/stats.module.js";
 
-// Adjust camera position
-camera.position.set(0, 4, 7); // Adjust x, y, z as needed
-camera.lookAt(new THREE.Vector3(0, 0, 0)); // Look at the center of the scene
+import { GUI } from "three/addons/libs/lil-gui.module.min.js";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { Water } from "three/addons/objects/Water.js";
+import { Sky } from "three/addons/objects/Sky.js";
+import { SceneUtils } from "three/examples/jsm/Addons";
 
-// Create a renderer
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+let container, stats;
+let camera, scene, renderer;
+let controls, water, sun, mesh;
 
-const orbit = new OrbitControls(camera, renderer.domElement);
-orbit.update();
+init();
+animate();
 
-const spotLight = new THREE.SpotLight(0xffffff, 1);
-spotLight.position.set(0, 4, 10);
-spotLight.angle = Math.PI / 4;
-spotLight.penumbra = 0.1;
-scene.add(spotLight);
+function init() {
+  container = document.getElementById("container");
 
-const cupMaterial = new THREE.MeshPhongMaterial({
-  color: 0x00ff00,
-  specular: 0x555555,
-  shininess: 30,
-});
-const ballMaterial = new THREE.MeshPhongMaterial({
-  color: 0xff0000,
-  specular: 0x111111,
-  shininess: 100,
-  transparent: true,
-});
+  // Renderer
 
-// Create geometry for the cups and ball
-const cupGeometry = new THREE.CylinderGeometry(0.5, 0.5, 1, 32);
-const ballGeometry = new THREE.SphereGeometry(0.2, 32, 32);
+  renderer = new THREE.WebGLRenderer();
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.toneMapping = THREE.ACESFilmicToneMapping;
+  renderer.toneMappingExposure = 0.5;
+  container.appendChild(renderer.domElement);
 
-// Create mesh for the cups
-const cups = [];
-for (let i = 0; i < 3; i++) {
-  const cup = new THREE.Mesh(cupGeometry, cupMaterial);
-  cup.position.x = i * 2 - 2;
-  cup.position.y = 0.5; // half the height of the cup
-  scene.add(cup);
-  cups.push(cup);
+  // Scene
+
+  scene = new THREE.Scene();
+
+  camera = new THREE.PerspectiveCamera(
+    55,
+    window.innerWidth / window.innerHeight,
+    1,
+    20000
+  );
+  camera.position.set(30, 30, 100);
+
+  // Sun
+
+  sun = new THREE.Vector3();
+
+  // Water
+
+  const waterGeometry = new THREE.PlaneGeometry(10000, 10000);
+
+  water = new Water(waterGeometry, {
+    textureWidth: 512,
+    textureHeight: 512,
+    waterNormals: new THREE.TextureLoader().load(
+      "images/waternormals.jpg",
+      function (texture) {
+        texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+      }
+    ),
+    sunDirection: new THREE.Vector3(),
+    sunColor: 0xffffff,
+    waterColor: 0x001e0f,
+    distortionScale: 3.7,
+    fog: scene.fog !== undefined,
+  });
+
+  water.rotation.x = -Math.PI / 2;
+  water.material.uniforms.size.value = 10;
+  water.material.uniforms.distortionScale.value = 3.7;
+
+  scene.add(water);
+
+  // Skybox
+
+  const sky = new Sky();
+  sky.scale.setScalar(10000);
+  scene.add(sky);
+
+  const skyUniforms = sky.material.uniforms;
+
+  skyUniforms["turbidity"].value = 10;
+  skyUniforms["rayleigh"].value = 2;
+  skyUniforms["mieCoefficient"].value = 0.005;
+  skyUniforms["mieDirectionalG"].value = 0.8;
+
+  const parameters = {
+    elevation: 2,
+    azimuth: 180,
+  };
+
+  const pmremGenerator = new THREE.PMREMGenerator(renderer);
+  const sceneEnv = new THREE.Scene();
+
+  let renderTarget;
+
+  function updateSun() {
+    const phi = THREE.MathUtils.degToRad(90 - parameters.elevation);
+    const theta = THREE.MathUtils.degToRad(parameters.azimuth);
+
+    sun.setFromSphericalCoords(1, phi, theta);
+
+    sky.material.uniforms["sunPosition"].value.copy(sun);
+    water.material.uniforms["sunDirection"].value.copy(sun).normalize();
+
+    if (renderTarget !== undefined) renderTarget.dispose();
+
+    sceneEnv.add(sky);
+    renderTarget = pmremGenerator.fromScene(sceneEnv);
+    scene.add(sky);
+
+    scene.environment = renderTarget.texture;
+  }
+
+  updateSun();
+
+  // Adding Middle Box
+
+  const geometry = new THREE.BoxGeometry(30, 30, 30);
+  const material = new THREE.MeshStandardMaterial({ roughness: 0 });
+
+  mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  // Orbit Controls
+
+  controls = new OrbitControls(camera, renderer.domElement);
+  controls.maxPolarAngle = Math.PI * 0.495;
+  controls.target.set(0, 10, 0);
+  controls.minDistance = 40.0;
+  controls.maxDistance = 200.0;
+  controls.update();
+
+  // Stats
+
+  stats = new Stats();
+  container.appendChild(stats.dom);
+
+  // GUI
+
+  const gui = new GUI();
+
+  gui.add(parameters, "azimuth", -180, 180, 0.1).onChange(updateSun);
+
+  window.addEventListener("resize", onWindowResize);
 }
 
-// Create mesh for the ball
-const ball = new THREE.Mesh(ballGeometry, ballMaterial);
-ball.position.y = 0.2; // half the height of the ball (radius)
-scene.add(ball);
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
 
-// Update renderer settings
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap; // Softer shadows
-renderer.antialias = true;
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
 
-// Enable shadows for light sources
-// pointLight.castShadow = true;
-spotLight.castShadow = true;
+function animate() {
+  requestAnimationFrame(animate);
+  render();
+  stats.update();
+}
 
-// Enable shadows for objects
-cups.forEach((cup) => (cup.castShadow = true));
-ball.castShadow = true;
+function render() {
+  water.material.uniforms["time"].value += 10.0 / 60.0;
+  renderer.render(scene, camera);
+}
 
-// Create a ground plane that acts as a floor
-const groundGeometry = new THREE.PlaneGeometry(20, 20); // Adjust size as needed
-const groundMaterial = new THREE.MeshPhongMaterial({
-  color: 0xcccccc,
-  specular: 0x101010,
-}); // Neutral color
+let cups, ball;
 
-const ground = new THREE.Mesh(groundGeometry, groundMaterial);
-ground.rotation.x = -Math.PI / 2; // Rotate to lay flat
-ground.position.y = 0; // Lower it below the cups and ball
-ground.receiveShadow = true; // Enable shadow reception
+function renderCup() {
+  const cup = new THREE.Object3D();
+  cup.name = "cup";
 
-scene.add(ground);
+  let cupBodyMaterials = [];
+  /*****************************************************
+   *   BODY                                            *
+   *****************************************************/
 
-let positions = [-2, 0, 2]; // Left, Middle, Right positions on the X-axis
+  const cupBodyGeometry = new THREE.CylinderGeometry(
+    2.646,
+    2.083,
+    6,
+    64,
+    12,
+    true
+  );
+  var loader2 = new THREE.TextureLoader();
+  loader2.crossOrigin = "*";
+
+  const cupBodyMaterial = new THREE.MeshBasicMaterial({
+    color: 0xffffff,
+    side: THREE.DoubleSide,
+    overdraw: 0.5,
+  });
+  cupBodyMaterial.name = "cupBodyMaterial";
+
+  const cupTexture = new THREE.TextureLoader().load("images/cup.jpg");
+
+  const cupBodyMaterialTexture = new THREE.MeshBasicMaterial({
+    map: cupTexture,
+    transparent: true,
+    overdraw: true,
+  });
+  cupBodyMaterialTexture.name = "cupBodyTexture";
+
+  cupBodyMaterials.push(cupBodyMaterial);
+  cupBodyMaterials.push(cupBodyMaterialTexture);
+
+  cupBodyMaterial.vertexColors = THREE.FaceColors;
+
+  const cupBodyMesh = SceneUtils.createMultiMaterialObject(
+    cupBodyGeometry,
+    cupBodyMaterials
+  );
+  cupBodyMesh.name = "cupBody";
+  cup.add(cupBodyMesh);
+
+  /*****************************************************
+   *   TOP                                             *
+   *****************************************************/
+
+  const cupTopGeometry = new THREE.TorusGeometry(1.29, 0.06, 0.16, 62);
+  const cupTopMaterial = new THREE.MeshStandardMaterial({
+    color: 0xffffff,
+    roughness: 0.5,
+    metalness: 0.5,
+    emissive: 0xc4c4c4,
+    overdraw: true,
+  });
+
+  const cupTop = new THREE.Mesh(cupTopGeometry, cupTopMaterial);
+  cupTop.name = "cupTop";
+  cupTop.position.set(0, 1.25, 0);
+  cupTop.rotation.x = Math.PI / 2;
+
+  cup.add(cupTop);
+
+  /*****************************************************
+   *   BOTTOM                                             *
+   *****************************************************/
+
+  const cupBottomGeometry = new THREE.CircleGeometry(2.083, 32); // Use the radius of the bottom of the cylinder
+  const cupBottomMaterial = new THREE.MeshBasicMaterial({
+    color: 0xc8cbcf,
+    side: THREE.DoubleSide,
+    overdraw: 0.5,
+  });
+
+  const cupBottomMesh = new THREE.Mesh(cupBottomGeometry, cupBottomMaterial);
+  cupBottomMesh.name = "cupBottom";
+  cupBottomMesh.position.set(0, -3, 0); // Position it at the bottom of the cylinder
+  cupBottomMesh.rotation.x = Math.PI / 2; // Rotate to align with the bottom
+
+  cup.add(cupBottomMesh);
+
+  // flip the cup so it's facing up
+  cup.rotation.x = Math.PI;
+
+  return cup;
+}
+
+function gameInit() {
+  const cupMaterial = new THREE.MeshStandardMaterial({
+    roughness: 0,
+    color: 0x00ff00,
+  });
+  const ballMaterial = new THREE.MeshStandardMaterial({
+    roughness: 0,
+    color: 0xff0000,
+  });
+
+  // Create geometry for the cups and ball
+  const cupGeometry = new THREE.CylinderGeometry(2.5, 2.5, 5, 32);
+  const ballGeometry = new THREE.SphereGeometry(1, 32, 32);
+
+  // Create mesh for the cups
+  cups = [];
+  for (let i = 0; i < 3; i++) {
+    const cup = renderCup();
+
+    cup.position.x = i * 8 - 8;
+    cup.position.y = 15 + 2.5; // half box height (30) + half the height of the cup (5)
+    cup.position.z = 0;
+
+    scene.add(cup);
+    cups.push(cup);
+  }
+
+  // Create mesh for the ball
+  ball = new THREE.Mesh(ballGeometry, ballMaterial);
+  ball.position.y = 15 + 1; // half box height (30) + half the height of the ball (radius = 1)
+  scene.add(ball);
+
+  // Update renderer settings
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap; // Softer shadows
+  renderer.antialias = true;
+
+  // Enable shadows for objects
+  cups.forEach((cup) => (cup.castShadow = true));
+  ball.castShadow = true;
+}
+
+gameInit();
+let positions = [-8, 0, 8]; // Left, Middle, Right positions on the X-axis
 let isSwitching = false;
 let switchDuration = 1000; // Duration of the switch in milliseconds
 let switchStartTime;
@@ -97,7 +308,7 @@ let switchQueue = [];
 let numberOfSwitches = 5; // Number of switches to perform at the start
 
 let revealDuration = 1000; // Duration to reveal the ball
-let revealHeight = 1; // Height to lift the cups
+let revealHeight = 5; // Height to lift the cups
 let ballRevealed = false;
 
 let ballUnderCupIndex = 1; // Index of the cup hiding the ball
@@ -106,6 +317,15 @@ ball.position.x = 0;
 
 let revealStartTime, revealEndTime;
 let isRevealing = false;
+
+function animateGame() {
+  if (!isRevealing) {
+    animateCups();
+  }
+  rayCaster.setFromCamera(mouse, camera);
+  renderer.render(scene, camera);
+}
+renderer.setAnimationLoop(animateGame);
 
 function revealBall() {
   ballRevealed = true;
@@ -122,7 +342,7 @@ function animateReveal() {
 
   if (deltaTime < revealDuration) {
     let fraction = deltaTime / revealDuration;
-    let currentHeight = 0.5 + fraction * revealHeight;
+    let currentHeight = 15 + 2.5 + fraction * revealHeight;
 
     cups.forEach((cup) => {
       cup.position.y = currentHeight;
@@ -142,7 +362,7 @@ function finishReveal() {
 
   if (deltaTime < revealDuration) {
     let fraction = deltaTime / revealDuration;
-    let currentHeight = revealHeight + 0.5 - fraction * revealHeight;
+    let currentHeight = revealHeight + 15 + 2.5 - fraction * revealHeight;
 
     cups.forEach((cup) => {
       cup.position.y = currentHeight;
@@ -158,9 +378,6 @@ function finishReveal() {
     startNextSwitch();
   }
 }
-
-// Start the game by revealing the ball
-revealBall();
 
 // Function to generate a series of random switches
 function generateSwitches() {
@@ -217,7 +434,7 @@ function animateCups() {
         positions[cupToSwitch2],
         positions[cupToSwitch1],
       ];
-      winningCupIndex = positions[ballUnderCupIndex] / 2 + 1;
+      winningCupIndex = positions[ballUnderCupIndex] / 8 + 1;
       startNextSwitch();
 
       // Start the next switch
@@ -228,15 +445,6 @@ function animateCups() {
       ball.position.z = cups[ballUnderCupIndex].position.z;
       ball.castShadow = false;
     }
-  }
-}
-
-function updateBallPosition() {
-  //not working
-  if (cupToSwitch1 === ballUnderCupIndex) {
-    ballUnderCupIndex = cupToSwitch2;
-  } else if (cupToSwitch2 === ballUnderCupIndex) {
-    ballUnderCupIndex = cupToSwitch1;
   }
 }
 
@@ -254,16 +462,6 @@ function checkGuess(index) {
 const rayCaster = new THREE.Raycaster();
 const mouse = new THREE.Vector2();
 
-// Call the animate function repeatedly
-function animate() {
-  if (!isRevealing) {
-    animateCups();
-  }
-  rayCaster.setFromCamera(mouse, camera);
-  renderer.render(scene, camera);
-}
-renderer.setAnimationLoop(animate);
-
 function positionToIndex(position) {
   return position == 2 ? 2 : position == 0 ? 1 : 0;
 }
@@ -279,15 +477,15 @@ document.addEventListener("click", function (e) {
   console.log(clickedCupIndex);
   const isCorrect = checkGuess(clickedCupIndex);
 
-  const resultElement = document.getElementById('guessResult');
-    resultElement.innerHTML = isCorrect ? "Correct!" : "Wrong Guess!";
-    resultElement.classList.add('show');
+  const resultElement = document.getElementById("guessResult");
+  resultElement.innerHTML = isCorrect ? "Correct!" : "Wrong Guess!";
+  resultElement.classList.add("show");
 
-    // Hide the message after a few seconds
-    setTimeout(() => {
-        resultElement.classList.remove('show');
-    }, 2000);
-    
+  // Hide the message after a few seconds
+  setTimeout(() => {
+    resultElement.classList.remove("show");
+  }, 2000);
+
   if (isCorrect) {
     console.log("Correct guess!");
     revealBall();
@@ -297,4 +495,6 @@ document.addEventListener("click", function (e) {
   }
 });
 
-animate();
+// Start the game by revealing the ball
+revealBall();
+animateGame();
